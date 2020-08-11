01e76848dffecbd51040efcd49f841fc69620d7f
sinaeYu <anne.sinae@gmail.com>
2020-08-11 23:29:29 +0900

    insert dummy data and check

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Nav from "components/Nav/Nav";
 import Cart from "components/Cart/Cart";
 import Main from "components/Main/Main";
+import Item from "components/Item/Item";
+import data from "MOCK_DATA.json";
 
-const App = props => [
-  <Nav key={1} />,
-  <Routes key={2} />
-];
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data
+    };
+  };
 
-const Routes = props => (
-  <Switch>
-    <Route exact path="/" component={Main} />
-    <Route exact path="/cart" component={Cart} />
-  </Switch>
-);
+  componentDidMount() {
+    console.log(this.state.products)
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/cart" component={Cart} />
+        </Switch>
+      </div>
+    );
+  };
+};
 
 
 export default App;
