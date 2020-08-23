9caaebf12080101586df7feab5f15da6c519c86e
sinaeYu <anne.sinae@gmail.com>
2020-08-24 07:51:39 +0900

    set sequelize, path, mysql2 and db.json

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
+
+const sequelize = require('./models').sequelize;
+sequelize.sync();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 4000;
-const db = require('./config/db');
-
-app.get('/api/products', (req, res) => {
-    db.query("SELECT * FROM mall_test", (err, data) => {
-        if(!err) res.send({ products : data });
-        else res.send(err);
-    })
-})
-
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
 })
